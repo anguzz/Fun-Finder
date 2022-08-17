6cf0a5a9c77589f7850114e7045a0fa0bff72406
Angel <santoyox714@gmail.com>
2022-08-17 13:17:06 -0700

    update

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -28,8 +28,8 @@
     width: 100%;
   height: 100vh;
   color: #fff;
-  background-image: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
-  background-size: 400% 400%;
+  background-image: url("bricks.background.jpg");
+  background-size: 120% 120%;
   position: relative;
   animation: change 10s ease-in-out infinite;
   }
